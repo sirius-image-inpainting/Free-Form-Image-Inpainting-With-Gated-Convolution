17d6702c7ef0a950830752dabb80dc86c0294b5e
Max Pershin <mepershin@gmail.com>
2021-03-20 15:19:17 +0300

    fix summary tool for layers

--- cmd/model_summary.py
+++ cmd/model_summary.py
@@ -1,9 +1,26 @@
 import torch
 import torchsummary
-import model.discriminator as d
 
-device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
-model = d.SNPatchGANDiscriminator().to(device)
+import model.layers
+import model.generator
+import model.discriminator
+
+
+def make_device():
+    if torch.cuda.is_available():
+        return torch.device('cuda')
+
+    return torch.device('cpu')
+
+
+def make_tensor(shape):
+    return torch.randn(*shape)
+
+
+device = make_device()
+m = model.discriminator.SNPatchGANDiscriminator()
 input_size = (5, 256, 256)
 
-torchsummary.summary(model, input_size)
+
+m = m.to(device)
+torchsummary.summary(m, input_size)

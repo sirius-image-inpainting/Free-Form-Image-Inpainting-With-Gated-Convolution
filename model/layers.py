8c8ebedbb80bdf78530655f2f5471fe06f4aed84
Max Pershin <mepershin@gmail.com>
2021-03-20 15:19:33 +0300

    self attention layer done

--- model/layers.py
+++ model/layers.py
@@ -128,3 +128,91 @@ class GatedConv2d(nn.Module):
         output = self.feature_act(feature) * self.gating_act(gating)
         return output
 
+
+
+class SelfAttention(nn.Module):
+
+    def __init__(self, in_channels: int,
+                       inter_channels: int = None):
+        """
+        Self attention layer. Originally described at
+        https://arxiv.org/pdf/1805.08318.pdf. Authors
+        of paper propose `inter_channels` = `in_channels` / 8.
+        This logic is implemented when `inter_channels` is None.
+
+        Parameters
+        ----------
+        in_channels : int
+            Channel count in input tensor.
+        out_channels : int
+            Channel count in  tensor.
+        """
+
+        super(SelfAttention, self).__init__()
+
+        if inter_channels is None:
+            inter_channels = in_channels // 8
+
+        self.in_channels = in_channels
+        self.inter_channels = inter_channels
+        self.gamma = nn.Parameter(torch.zeros(1))
+
+        self.conv_key = nn.Conv2d(in_channels=in_channels,
+                                  out_channels=inter_channels,
+                                  kernel_size=1)
+
+        self.conv_query = nn.Conv2d(in_channels=in_channels,
+                                    out_channels=inter_channels,
+                                    kernel_size=1)
+
+        self.conv_value = nn.Conv2d(in_channels=in_channels,
+                                    out_channels=inter_channels,
+                                    kernel_size=1)
+
+        self.conv_final = nn.Conv2d(in_channels=inter_channels,
+                                    out_channels=in_channels,
+                                    kernel_size=1)
+
+
+    def forward(self, X: torch.Tensor) -> torch.Tensor:
+        # saving dimensions
+        batch_size = X.shape[0]     # refered as B
+        channel_count = X.shape[1]  # refered as C
+        height = X.shape[2]         # refered as H
+        width = X.shape[3]          # refered as W
+
+        # computing key, query and value
+        # (B, c, H, W)
+        key = self.conv_key(X)
+        query = self.conv_query(X)
+        value = self.conv_value(X)
+
+        # resizing matrices (B, c, H, W) -> (B, c, H * W)
+        # (`inter_channels` is refered as c)
+        key = key.view(batch_size, self.inter_channels, height * width)
+        query = query.view(batch_size, self.inter_channels, height * width)
+        value = value.view(batch_size, self.inter_channels, height * width)
+
+        # transposing query matrix
+        query = query.permute(0, 2, 1) # (B, c, H * W) -> (B, H * W, c)
+
+        # multiplying key and value to get attention scores
+        attention = torch.bmm(query, key) # (B, H * W, H * W)
+
+        # normalizing each column of attention score with softmax
+        # (column normalization is used not to transpose afterwards)
+        attention = torch.softmax(attention, dim=1)
+
+        # multiplying values and attention scores, reshaping
+        # (B, c, H * W) x (B, H * W, H * W) -> (B, c, H * W) -> (B, c, H, W)
+        att_value = torch.bmm(value, attention)
+        att_value = att_value.view(batch_size, self.inter_channels, height, width)
+
+        # going through final convolution
+        # (B, c, H, W) -> (B, C, H, W)
+        result = self.conv_final(att_value)
+
+        # multiplying with gamma parameter
+        result = self.gamma * result + X
+        return result
+

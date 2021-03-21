332d435ab5dcf77e76cca3c41e6bf6aff5218235
Max Pershin <mepershin@gmail.com>
2021-03-21 11:11:33 +0300

    generator forward step done

--- model/generator.py
+++ model/generator.py
@@ -117,6 +117,7 @@ class SNPatchGANGenerator(nn.Module):
             gated_upconv2d(64, 32, 3, 1, 1),            # layer 15 (64 x 128 x 128) -> (32 x 256 x 256)
             gated_conv2d(32, 16, 3, 1, 1),              # layer 16 (32 x 256 x 256) -> (16 x 256 x 256)
             gated_conv2d(16, 3, 3, 1, 1),               # layer 17 (16 x 256 x 256) -> (3 x 256 x 256)
+            nn.Tanh(),                                  # layer 18 (3 x 256 x 256)  -> (3 x 256 x 256)
         )
 
         self.refine_conv = nn.Sequential(
@@ -144,13 +145,56 @@ class SNPatchGANGenerator(nn.Module):
             gated_conv2d(128, 128, 3, 1, 1),            # layer 09 (128 x 64 x 64)  -> (128 x 64 x 64)
         )
 
-        #  self.refine_tail = nn.Sequential(
-
-        #  )
+        self.refine_tail = nn.Sequential(
+            gated_conv2d(256, 128, 3, 1, 1),            # layer 01 (256 x 64 x 64)  -> (128 x 64 x 64)
+            gated_conv2d(128, 128, 3, 1, 1),            # layer 02 (128 x 64 x 64)  -> (128 x 64 x 64)
+            gated_upconv2d(128, 64, 3, 1, 1),           # layer 03 (128 x 64 x 64)  -> (64 x 128 x 128)
+            gated_conv2d(64, 64, 3, 1, 1),              # layer 04 (64 x 128 x 128) -> (64 x 128 x 128)
+            gated_upconv2d(64, 32, 3, 1, 1),            # layer 05 (64 x 128 x 128) -> (32 x 256 x 256)
+            gated_conv2d(32, 16, 3, 1, 1),              # layer 06 (32 x 256 x 256) -> (16 x 256 x 256)
+            gated_conv2d(16, 3, 3, 1, 1),               # layer 07 (16 x 256 x 256) -> (3 x 256 x 256)
+            nn.Tanh(),                                  # layer 08 (3 x 256 x 256)  -> (3 x 256 x 256)
+        )
 
 
     def forward(self, X: torch.Tensor) -> torch.Tensor:
-        #  X = self.coarse(X)
-        X_1 = self.refine_attention(X)
-        return X_1
+        """
+        Forward step for generator module.
+
+        Parameters
+        ----------
+        X : torch.Tensor
+            Torch tensor of shape (B, 4, 256, 256), where B is batch size.
+            First three channels - image channels, each pixel in [-1, 1].
+            Last channel - mask channel, each pixel in {0, 1}.
+        """
+
+        batch_size = X.shape[0]
+        channels = X.shape[1]
+        height = X.shape[2]
+        width = X.shape[3]
+
+        # sanity check
+        assert channels == 4
+
+        # fetching masks and images
+        image_masks = X[:, 3, : ,:].view(batch_size, 1, height, width)
+        images = X[:, 0:3, :, :]
+
+        # masking input images
+        masked_images = images * (1 - image_masks)
+        input_tensor = torch.cat([masked_images, image_masks], dim=1)
+
+        # step 1: coarse reconstruct
+        X_coarse = self.coarse(input_tensor)
+        X_rec_empty = images * (1 - image_masks) + X_coarse * image_masks
+
+        # step 2: refinement
+        X_rec_with_masks = torch.cat([X_rec_empty, image_masks], dim=1)
+        X_refine_b1 = self.refine_conv(X_rec_with_masks)
+        X_refine_b2 = self.refine_attention(X_rec_with_masks)
+        X_refine_all = torch.cat([X_refine_b1, X_refine_b2], dim=1)
+        X_refine_out = self.refine_tail(X_refine_all)
+
+        return X_refine_out
 

5d53a26292b53a56da9f16993c9329d4b8bf9e3f
Max Pershin <mepershin@gmail.com>
2021-03-22 10:40:57 +0300

    mask generating utility done

--- model/data.py
+++ model/data.py
@@ -26,8 +26,10 @@ TEST_DATASET_ROOT = './data/test/'
 
 def generate_random_mask(height: int = 256,
                          width: int = 256,
-                         min_edges: int = 4,
-                         max_edges: int = 12,
+                         min_lines: int = 1,
+                         max_lines: int = 4,
+                         min_vertex: int = 5,
+                         max_vertex: int = 13,
                          mean_angle: float = 2/5 * math.pi,
                          angle_range: float = 2/15 * math.pi,
                          min_width: float = 12,
@@ -42,10 +44,14 @@ def generate_random_mask(height: int = 256,
         Height of mask.
     width : int
         Width of mask.
-    min_edges : int
-        Minimal count of edges to draw.
-    max_edges : int
-        Maximum count of edges to draw.
+    min_lines : int
+        Miniumal count of lines to draw on mask.
+    max_lines : int
+        Maximal count of lines to draw on mask.
+    min_vertex : int
+        Minimal count of vertexes to draw.
+    max_vertex : int
+        Maximum count of vertexes to draw.
     mean_angle : float
         Mean value of angle between edges.
     angle_range : float
@@ -56,48 +62,66 @@ def generate_random_mask(height: int = 256,
         Maximum width of edge to draw.
     """
 
-    average_radius = math.sqrt(height * height + width * width) / 8
-    mask = Image.new('L', (width, height), 0)
+    # init mask and drawing tool
+    mask = Image.new('1', (width, height), 0)
+    draw = ImageDraw.Draw(mask)
 
-    for _ in range(np.random.randint(1, 4)):
-        num_vertex = np.random.randint(min_edges, max_edges)
+    # calculate mean radius to draw lines and count of lines
+    num_lines = np.random.randint(min_lines, max_lines)
+    average_radius = math.sqrt(height * height + width * width) / 8
+
+    # drawing lines
+    for _ in range(num_lines):
         angle_min = mean_angle - np.random.uniform(0, angle_range)
         angle_max = mean_angle + np.random.uniform(0, angle_range)
+        num_vertex = np.random.randint(min_vertex, max_vertex)
+
+        # line parameters
         angles = []
         vertex = []
-        for i in range(num_vertex):
-            if i % 2 == 0:
-                angles.append(2*math.pi - np.random.uniform(angle_min, angle_max))
-            else:
-                angles.append(np.random.uniform(angle_min, angle_max))
 
-        h, w = mask.size
-        vertex.append((int(np.random.randint(0, w)), int(np.random.randint(0, h))))
-        for i in range(num_vertex):
-            r = np.clip(
-                np.random.normal(loc=average_radius, scale=average_radius//2),
-                0, 2*average_radius)
-            new_x = np.clip(vertex[-1][0] + r * math.cos(angles[i]), 0, w)
-            new_y = np.clip(vertex[-1][1] + r * math.sin(angles[i]), 0, h)
+        # generating line angles
+        for i in range(num_vertex - 1):
+            random_angle = np.random.uniform(angle_min, angle_max)
+            if i % 2 == 0:
+                random_angle = 2 * np.pi - random_angle
+            angles.append(random_angle)
+
+        # start point
+        start_x = np.random.randint(0, width)
+        start_y = np.random.randint(0, height)
+        vertex.append((start_x, start_y))
+
+        # generating next points
+        for i in range(num_vertex - 1):
+            radius = np.random.normal(loc=average_radius, scale=average_radius / 2)
+            radius = np.clip(radius, 0, 2 * average_radius)
+            new_x = np.clip(vertex[-1][0] + radius * math.cos(angles[i]), 0, width)
+            new_y = np.clip(vertex[-1][1] + radius * math.sin(angles[i]), 0, height)
             vertex.append((int(new_x), int(new_y)))
 
-        draw = ImageDraw.Draw(mask)
-        width = int(np.random.uniform(min_width, max_width))
-        draw.line(vertex, fill=1, width=width)
-        for v in vertex:
-            draw.ellipse((v[0] - width//2,
-                          v[1] - width//2,
-                          v[0] + width//2,
-                          v[1] + width//2),
-                         fill=1)
+        # drawing line
+        line_width = np.random.uniform(min_width, max_width)
+        line_width = int(line_width)
+        draw.line(vertex, fill=1, width=line_width)
 
+        # smoothing angles
+        for node in vertex:
+            x_ul = node[0] - line_width // 2
+            x_br = node[0] + line_width // 2
+            y_ul = node[1] - line_width // 2
+            y_br = node[1] + line_width // 2
+            draw.ellipse((x_ul, y_ul, x_br, y_br), fill=1)
+
+    # random vertical flip
     if np.random.normal() > 0:
         mask.transpose(Image.FLIP_LEFT_RIGHT)
+
+    # random horizontal flip
     if np.random.normal() > 0:
         mask.transpose(Image.FLIP_TOP_BOTTOM)
 
     mask = np.asarray(mask, np.float32)
-    mask = np.reshape(mask, (1, height, width, 1))
     return mask
 
 
@@ -155,14 +179,14 @@ class PlacesDataset(torch.utils.data.Dataset):
         """
 
         filename = self.files[index]
-        image = cv2.imread(filename).astype(numpy.float)
+        image = cv2.imread(filename).astype(np.float)
 
         if self.transform:
             image = (image / 255) * 2 - 1
 
         if self.make_mask:
             mask = generate_random_mask(width=256, height=256)
-            image = numpy.concatenate([image, mask], axis=1)
+            image = np.concatenate([image, mask], axis=1)
 
         image = torch.from_numpy(image)
         return image
